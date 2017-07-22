eebde226ad500ae0f2de2f38dcd82e0dc048e0ba
Ankur Srivastava <ankur.srivastava@recogizer.de>
2017-07-22 15:45:02 +0200

    cleanup funcitons

--- src/manager.rs
+++ src/manager.rs
@@ -1,39 +1,49 @@
 use clap::{App, Arg, SubCommand};
 use saver::ImageSaver;
 
-pub fn run() {
 
-    let volume_save = Arg::with_name("volume")
+fn get_path_arg(help: &str) -> Arg {
+
+    Arg::with_name("path")
+        .short("p")
+        .long("path")
+        .help(help)
+        .takes_value(true)
+        .default_value(".")
+        .required(true)
+}
+
+fn get_volume_arg(help: &str) -> Arg {
+    Arg::with_name("volume")
         .short("v")
         .long("volume")
-        .help("The named volume to save locally.")
+        .help(help)
         .takes_value(true)
-        .required(true);
+        .required(true)
+}
 
+fn get_interactive_arg(help: &str) -> Arg {
 
-    let volume_load = Arg::with_name("volume")
-        .short("v")
-        .long("volume")
-        .help("The target name of volume where the zip is to be uploaded.")
-        .takes_value(true)
-        .required(true);
-
-    let interactive = Arg::with_name("interactive")
+    Arg::with_name("interactive")
         .short("i")
         .long("interactive")
-        .help("Run the command interactively")
+        .help(help)
         .takes_value(true)
         .default_value("true")
         .possible_values(&["true", "false"])
-        .required(true);
+        .required(true)
+}
 
-    let path = Arg::with_name("path")
-        .short("p")
-        .long("path")
-        .help("The path to save the docker volume as .tar.gz")
-        .takes_value(true)
-        .default_value(".")
-        .required(true);
+
+pub fn run() {
+
+    let volume_save = get_volume_arg("The named volume to save locally.");
+    let volume_load = get_volume_arg("The target name of volume where the zip is to be uploaded.");
+
+    let path_save = get_path_arg("The path to save the docker volume as .tar.gz");
+    let path_load = get_path_arg("The path to load the docker volume from");
+
+    let interactive = get_interactive_arg("Run the command interactively");
 
     let app = App::new("dvm")
         .version("0.1.0")
@@ -41,12 +51,12 @@ pub fn run() {
         .subcommand(SubCommand::with_name("save")
                         .about("Save your docker volume locally.")
                         .arg(volume_save)
-                        .arg(&path)
+                        .arg(path_save)
                         .arg(&interactive))
         .subcommand(SubCommand::with_name("load")
                         .about("Load the locally saved volume to named docker-volume.")
                         .arg(volume_load)
-                        .arg(&path)
+                        .arg(path_load)
                         .arg(&interactive));
 
     let mut out = Vec::new();
